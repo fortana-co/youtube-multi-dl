8096a096d9f66f02212182fc10aa247d18131aa7
Kyle Bebak <kylebebak@gmail.com>
2019-02-12 01:23:47 -0600

    Adds handling of monolithic files with chapters

--- youtube_dl_playlist/downloader.py
+++ youtube_dl_playlist/downloader.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import subprocess
 
 import youtube_dl
 from mutagen.easyid3 import EasyID3
@@ -27,11 +28,11 @@ def downloader(url, artist, album='', playlist_items='', keep_id=False):
         sys.exit("couldn't get playlist info")
 
     album = album or info['title']
-    directory = f"./{album}"
+    directory = './{}'.format(album)
     try:
         os.makedirs(directory)
     except FileExistsError as e:
-        sys.exit(f"{e}\n\nyoutube-dl-playlist can't overwrite existing directories")
+        sys.exit("{}\n\nyoutube-dl-playlist can't overwrite existing directories".format(e))
     os.chdir(directory)
 
     with youtube_dl.YoutubeDL(download_opts) as ydl:
@@ -43,15 +44,46 @@ def downloader(url, artist, album='', playlist_items='', keep_id=False):
                 track_info = ydl.extract_info(entry['id'], download=False)
             if not track_info:
                 continue
-            for file in glob.glob(f"*{track_info['id']}.mp3"):
-                audio = EasyID3(file)
-                audio['title'] = track_info['title']
-                audio['artist'] = artist
-                audio['album'] = album
-                audio['tracknumber'] = f"{i + 1}/{len(info.get('entries'))}"
-                audio.save()
+            for file in glob.glob('*{}.mp3'.format(track_info['id'])):
+                set_audio_id3(
+                    file,
+                    title=track_info['title'],
+                    artist=artist,
+                    album=album,
+                    tracknumber='{}/{}'.format(i + 1, len(info.get('entries'))),
+                )
                 if not keep_id:
                     os.rename(
                         file,
-                        ''.join(file.split(f"-{track_info['id']}")),
+                        ''.join(file.split('-{}'.format(track_info['id']))),
                     )
+
+    if info.get('extractor') == 'youtube':
+        big_file = glob.glob('*{}.mp3'.format(info['id']))[0]
+        chapters = info.get('chapters')
+        if not chapters:
+            return
+
+        for i, chapter in enumerate(chapters):
+            start_time = chapter['start_time']
+            end_time = chapter['end_time']
+            title = chapter.get('title') or str(i + 1)
+            file = '{}.mp3'.format(title)
+            command = [
+                'ffmpeg', '-i', big_file, '-acodec', 'copy', '-ss', str(start_time), '-to', str(end_time), file,
+            ]
+            subprocess.check_output(command)
+            set_audio_id3(
+                file,
+                title=title,
+                artist=artist,
+                album=album,
+                tracknumber='{}/{}'.format(i + 1, len(chapters)),
+            )
+
+
+def set_audio_id3(file, **kwargs):
+    audio = EasyID3(file)
+    for k, v in kwargs.items():
+        audio[k] = v
+    audio.save()
